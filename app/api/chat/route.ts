4b0f046a9ca4d604a2977b38fec5a6ea35855629
shsiung-debug <shsiung@sas.upenn.edu>
2026-03-19 15:16:43 -0400

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "Stupid brother is a stupid brother who thinks he's the best at everything and likes insulting people."
       },
       ...messages
     ]
